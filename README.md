ff87bd1484a7644b3ed4fe72c4210cd39b5bd1a2
yutatokoi <69610953+yutatokoi@users.noreply.github.com>
2023-12-12 11:04:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# README
+
+Yuta Tokoi's personal portfolio website
+
+## Acknowledgment
+
+The template used is [Astro Cactus by @chrismwilliams](https://github.com/chrismwilliams/astro-theme-cactus)
+
+## License
+
+MIT
---
+++ src/site.config.ts
@@ -0,0 +1,42 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+	author: "Yuta Tokoi",
+	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+	title: "Yuta Tokoi",
+	// Meta property used as the default description meta property
+	description: "The portfolio/website of Yuta Tokoi",
+	// HTML lang property, found in src/layouts/Base.astro L:18
+	lang: "en-GB",
+	// Meta property, found in src/components/BaseHead.astro L:42
+	ogLocale: "en_GB",
+	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+	date: {
+		locale: "en-GB",
+		options: {
+			day: "numeric",
+			month: "short",
+			year: "numeric",
+		},
+	},
+	webmentions: {
+		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+	},
+};
+
+// Used to generate links in both the Header & Footer.
+export const menuLinks: Array<{ title: string; path: string }> = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "About",
+		path: "/about/",
+	},
+	{
+		title: "Blog",
+		path: "/posts/",
+	},
+];
